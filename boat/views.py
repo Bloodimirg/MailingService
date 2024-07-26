2eb3eab9c1512e5792bcfc12af3af9022b942414
Bloodimirg <bloodimirg@gmail.com>
2024-07-27 00:00:51 +0300

    созданы первые контроллеры

--- boat/views.py
+++ boat/views.py
@@ -1,3 +1,70 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from .models import Client, Mailing
+
+
+class HomePageView(TemplateView):
+    template_name = 'boat/home.html'
+
+
+class ClientListView(ListView):
+    model = Client
+    template_name = 'clients/client_list.html'
+
+
+class ClientDetailView(DetailView):
+    model = Client
+    template_name = 'clients/client_detail.html'
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = ['email', 'full_name', 'comment']
+    template_name = 'clients/client_form.html'
+    success_url = reverse_lazy('boat:client-list')
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    fields = ['email', 'full_name', 'comment']
+    template_name = 'clients/client_form.html'
+    success_url = reverse_lazy('boat:client-list')
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    template_name = 'clients/client_confirm_delete.html'
+    success_url = reverse_lazy('boat:client-list')
+
+
+# Views for Mailing
+class MailingListView(ListView):
+    model = Mailing
+    template_name = 'mailings/mailing_list.html'
+
+
+class MailingDetailView(DetailView):
+    model = Mailing
+    template_name = 'mailings/mailing_detail.html'
+
+
+class MailingCreateView(CreateView):
+    model = Mailing
+    fields = ['first_send_time', 'periodicity', 'status', 'message', 'clients']
+    template_name = 'mailings/mailing_form.html'
+    success_url = reverse_lazy('boat:mailing-list')
+
+
+class MailingUpdateView(UpdateView):
+    model = Mailing
+    fields = ['first_send_time', 'periodicity', 'status', 'message', 'clients']
+    template_name = 'mailings/mailing_form.html'
+    success_url = reverse_lazy('boat:mailing-list')
+
+
+class MailingDeleteView(DeleteView):
+    model = Mailing
+    template_name = 'mailings/mailing_confirm_delete.html'
+    success_url = reverse_lazy('boat:mailing-list')
