47bf116d2aacd0b9c37591dfefade9a565bbbcde
Bloodimirg <bloodimirg@gmail.com>
2024-07-27 00:00:06 +0300

    прописаны пути к шаблонам

--- boat/urls.py
+++ boat/urls.py
@@ -1,9 +1,24 @@
 from django.urls import path
 
 from boat.apps import BoatConfig
+from boat.views import ClientListView, ClientDetailView, ClientCreateView, ClientUpdateView, ClientDeleteView, \
+    MailingListView, MailingDetailView, MailingCreateView, MailingUpdateView, MailingDeleteView, HomePageView
 
 app_name = BoatConfig.name
 
 urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
 
+    path('clients/', ClientListView.as_view(), name='client-list'),
+    path('clients/<int:pk>/', ClientDetailView.as_view(), name='client-detail'),
+    path('clients/create/', ClientCreateView.as_view(), name='client-create'),
+    path('clients/<int:pk>/update/', ClientUpdateView.as_view(), name='client-update'),
+    path('clients/<int:pk>/delete/', ClientDeleteView.as_view(), name='client-delete'),
+
+    # Mailing URLs
+    path('mailings/', MailingListView.as_view(), name='mailing-list'),
+    path('mailings/<int:pk>/', MailingDetailView.as_view(), name='mailing-detail'),
+    path('mailings/create/', MailingCreateView.as_view(), name='mailing-create'),
+    path('mailings/<int:pk>/update/', MailingUpdateView.as_view(), name='mailing-update'),
+    path('mailings/<int:pk>/delete/', MailingDeleteView.as_view(), name='mailing-delete'),
 ]
