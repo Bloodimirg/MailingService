6d710c776eb0ba79c0978d4f8d24fe72f06e2ba3
Bloodimirg <bloodimirg@gmail.com>
2024-07-28 03:56:18 +0300

    Добавлены  Views для Message, Dashboard

--- boat/views.py
+++ boat/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
 
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
-from .models import Client, Mailing
+from .models import Client, Mailing, Message
 
 
 class HomePageView(TemplateView):
     template_name = 'boat/home.html'
 
 
+# CBV для клиентов
 class ClientListView(ListView):
     model = Client
     template_name = 'clients/client_list.html'
@@ -38,11 +38,7 @@ class ClientDeleteView(DeleteView):
     template_name = 'clients/client_confirm_delete.html'
     success_url = reverse_lazy('boat:client-list')
 
-
-# Views for Mailing
-class MailingListView(ListView):
-    model = Mailing
-    template_name = 'mailings/mailing_list.html'
+    # CBV для рассылок
 
 
 class MailingDetailView(DetailView):
@@ -54,17 +50,54 @@ class MailingCreateView(CreateView):
     model = Mailing
     fields = ['first_send_time', 'periodicity', 'status', 'message', 'clients']
     template_name = 'mailings/mailing_form.html'
-    success_url = reverse_lazy('boat:mailing-list')
+    success_url = reverse_lazy('boat:dashboard')
 
 
 class MailingUpdateView(UpdateView):
     model = Mailing
     fields = ['first_send_time', 'periodicity', 'status', 'message', 'clients']
     template_name = 'mailings/mailing_form.html'
-    success_url = reverse_lazy('boat:mailing-list')
+    success_url = reverse_lazy('boat:dashboard')
 
 
 class MailingDeleteView(DeleteView):
     model = Mailing
     template_name = 'mailings/mailing_confirm_delete.html'
-    success_url = reverse_lazy('boat:mailing-list')
+    success_url = reverse_lazy('boat:dashboard')
+
+    # CBV для писем
+
+
+class MessageDetailView(DetailView):
+    model = Message
+    template_name = 'message/message_detail.html'
+
+
+class MessageCreateView(CreateView):
+    model = Message
+    fields = ['subject', 'body']
+    template_name = 'message/message_form.html'
+    success_url = reverse_lazy('boat:dashboard')
+
+
+class MessageUpdateView(UpdateView):
+    model = Message
+    fields = ['subject', 'body']
+    template_name = 'message/message_form.html'
+    success_url = reverse_lazy('boat:dashboard')
+
+
+class MessageDeleteView(DeleteView):
+    model = Message
+    template_name = 'message/message_confirm_delete.html'
+    success_url = reverse_lazy('boat:dashboard')
+
+
+class DashboardView(TemplateView):
+    template_name = 'boat/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['mailings'] = Mailing.objects.all()
+        context['message'] = Message.objects.all()
+        return context
